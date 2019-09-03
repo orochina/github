cbfa4225751cea9a47cca19e66fb22eac45ed3d5
orochina <nking07@163.com>
2019-09-03 18:48:25 +0800

    myfirt cpp

---
+++ c++/myfirst.cpp
@@ -0,0 +1,12 @@
+//myfirst cpp displays a message 
+
+#include<iostream>                   //a preprocessor directive
+int main()                           //function header
+{                                    //start of function body
+    using namespace std;             //make definitions visible
+    cout<<"Come up and C++ me some time.";  //message
+    cout<<endl;
+    cout<<"You won't regret it!"<<endl;    //more ouput
+    return 0;                        //terminate main()
+}                                    //end of function body
+    
