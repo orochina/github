26e54991f35b6b391f40a8f37720a7ed1e521ab2
orochia <nking07@163.com>
2019-09-09 22:35:34 +0800

    3_2

--- c++/exceed.cpp
+++ c++/exceed.cpp
@@ -1,5 +1,5 @@
 // exceed.cpp --exceeding some integer limits
-#include <iosterm> 
+#include <iostream> 
 #define ZERO 0                      //makes ZERO symbol for 0 value
 #include <climits>                  //defines INT_MAX as largest int values
 int main()
@@ -12,7 +12,17 @@ int main()
 	cout << " dollars deposited." << endl
 	     << "Add $1 to each account." << endl << "Now ";
 	sam = sam + 1;
-	sue = sue + 1;
-	cout << "" 
-	
+	sue = sue+1;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited.\nPoor Sam!" << endl;
+	sam = ZERO;
+	sue = ZERO;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited. " << endl;
+	cout << "Take $1 from each account." << endl << "Now ";
+	sam = sam-1;
+	sue = sue -1;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited." << endl << "Lucky Sue!" << endl;
+	return 0;
 }
