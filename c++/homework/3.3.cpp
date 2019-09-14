3eaa7b8f28894f03de73ada5daadec9f717f5c93
orochina <nking07@163.com>
2019-09-14 11:23:56 +0800

    3.3

--- c++/homework/3.3.cpp
+++ c++/homework/3.3.cpp
@@ -1,13 +1,14 @@
 //degree minute second
 #include <iostream>
 using namespace std;
+
 int main()
 {
-	const int convert = 60;
+	const double convert = 60;
 	int mdegrees ;
 	int mminutes ;
 	int mseconds ;
-
+   
 	cout << "Enter a latitude in degree, minute, and second: " << endl;
 	cout << "First, enter the degrees: "; 
     cin >> mdegrees ;
@@ -15,7 +16,8 @@ int main()
     cin >> mminutes ;
     cout << "Finally, enter the seconds of arc: " ;
     cin >> mseconds ;
-    double mlatitude = mdegrees + mminutes/convert + mseconds/convert/convert ;
+    double mlatitude ;
+	mlatitude= mdegrees + mminutes/convert + mseconds/convert/convert ;
 	cout << mdegrees << " degrees £¬" << mminutes << " seconds ," << mseconds << " seconds = " 
 	     << mlatitude << " degrees." ;
 	return 0;
