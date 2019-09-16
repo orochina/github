c2f623dfb06ba8e345b1d60d8b653fd38be6db8e
orochina <nking07@163.com>
2019-09-16 17:57:32 +0800

    4.2string

---
+++ c++/4.11.cpp
@@ -0,0 +1,15 @@
+//a little try
+#include <iostream>
+#include <cstring>
+int main()
+{
+	using namespace std;
+	cout << "I'd give my right arm to be"     " a great violinist.\n";
+	cout << "I'd give my right arm to be a great violinist.\n";
+	cout << "I'd give my right ar"
+	
+	
+	"m to be a great violinist.\n";
+	return 0;
+}
+
