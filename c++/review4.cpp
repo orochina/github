7089fd111b705a03594b9b617531f717cbc25256
orochina <nking07@163.com>
2019-09-24 19:08:55 +0800

    review

---
+++ c++/review4.cpp
@@ -0,0 +1,48 @@
+//  µÚ4ÕÂÁ·Ï°Ìâ
+char actors [30] ;
+short betsie [100] ;
+
+array<char,30> actor;
+array<short,100> betsie;
+
+int apples [5] = {1 ,3 ,5 , 7, 9};
+
+int even= apples[0] + apples[4];
+
+cout << ideas[1] << endl;
+
+char lunch[] = "cheeseburger";
+
+string lunch = "Waldorf Salad";
+
+struct fish 
+{
+	char kind [];
+	int weight ;
+	float length;
+};
+
+fish petes =
+{
+	"trout",
+	12,
+	26.25
+};
+
+enum Response {No, Yes ,Maybe};
+
+double * pd = sted;
+cout << *pd <<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
