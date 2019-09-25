96fca37572cfeb68dfec96afe94402a506594b9a
orochina <nking07@163.com>
2019-09-25 20:08:26 +0800

    for loops

---
+++ c++/5.1.cpp
@@ -0,0 +1,12 @@
+// forloop.cpp -- introducing the for loop
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int i;             // create a counter
+// initilize ; test update
+    for (i =0; i < 5; i++)
+        cout << "C++ knows loops.\n";
+    cout << "C++ konws when to stop.\n";
+    return 0;
+}
